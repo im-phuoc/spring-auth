87a112d865c0087e1903c3e8273c9b3fe2d51f16
im-phuoc <pphan720@gmail.com>
2025-03-24 20:26:23 +0700

    chore: remove frontend files and configurations for a clean slate

--- frontend/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import LoginForm from "@/components/auth/LoginForm";
-export default function Home() {
-  return (
-    <div>
-      <LoginForm />
-    </div>
-  );
-}
